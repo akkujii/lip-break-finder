5851397388b4749e731e3cb251b3eba05262ea69
akkuJii <peinola+gh@gmail.com>
2024-11-05 21:15:16 +0200

    initial implementation to add ascending lip breaks for note. Support also to find lip breaks when same note has lip breaks on multiple partials.

--- src/LipBreakFinder.ts
+++ src/LipBreakFinder.ts
@@ -1,14 +1,35 @@
 import { TromboneNote, TromboneNoteMap } from "./notes";
 
 export interface ILipBreakFinder {
-  getDescendingLipBreakTonesWithSlidePositionsForPartial(partial: number): TromboneNote[] 
-  getAscendingLipBreakTonesWithSlidePositionsForPartial(partial: number): TromboneNote[] 
+  getDescendingLipBreakTonesWithSlidePositionsForPartial(partial: number): TromboneNote[]
+  getAscendingLipBreakTonesWithSlidePositionsForPartial(partial: number): TromboneNote[]
 }
 export class LipBreakFinder {
 
   private static notesAndPartials = TromboneNoteMap
 
   constructor() { }
+
+  getAscendingLipBreaksForNote(note: string) {
+
+    const ret: {
+      slidePositionOfStartingNote: number
+      lipBreaks: TromboneNote[]
+    }[] = [];
+
+    LipBreakFinder.notesAndPartials.forEach((notes: TromboneNote[], partialKey) => {
+      notes.forEach((tromboneNote) => {
+        if (tromboneNote.noteName === note) {
+          ret.push({
+            slidePositionOfStartingNote: tromboneNote.slidePosition,
+            lipBreaks: this.getAscendingLipBreakTonesWithSlidePositionsForPartial(partialKey)
+          })
+        }
+      })
+    })
+
+    return ret
+  }
 
   getAscendingLipBreakTonesWithSlidePositionsForPartial(partial: number): TromboneNote[] {
     return LipBreakFinder.notesAndPartials.get(partial + 1) ?? [];
--- tests/LipBreakFinder.test.ts
+++ tests/LipBreakFinder.test.ts
@@ -59,4 +59,73 @@ describe('LipBreakFinder', () => {
       .toEqual(expect.arrayContaining(expectedEleventhPartialNotes));
 
   });
+
+  describe('when note has lip breaks on one partial', () => {
+
+    it('should find the possible ascending lip breaks for the given note', () => {
+      const libBreakFinder = new LipBreakFinder();
+
+      const expectedThirdPartialnotes = [
+        { noteName: 'F3', slidePosition: 1 },
+        { noteName: 'E3', slidePosition: 2 },
+        { noteName: 'Eb3', slidePosition: 3 },
+        { noteName: 'D3', slidePosition: 4 },
+        { noteName: 'C#3', slidePosition: 5 },
+        { noteName: 'C3', slidePosition: 6 },
+        { noteName: 'B2', slidePosition: 7 }
+      ]
+
+      expect(libBreakFinder.getAscendingLipBreaksForNote('Bb2'))
+        .toEqual(expect.arrayContaining([
+          {
+            slidePositionOfStartingNote: 1,
+            lipBreaks: expectedThirdPartialnotes
+          }
+        ]));
+
+    });
+
+  })
+
+  describe('when note has lip breaks on multiple partials', () => {
+
+    it('should find the possible ascending lip breaks for the given note', () => {
+      const libBreakFinder = new LipBreakFinder();
+
+      const expectedFourthPartialNotes = [
+        { noteName: 'Bb3', slidePosition: 1 },
+        { noteName: 'A3', slidePosition: 2 },
+        { noteName: 'Ab3', slidePosition: 3 },
+        { noteName: 'G3', slidePosition: 4 },
+        { noteName: 'F#3', slidePosition: 5 },
+        { noteName: 'F3', slidePosition: 6 },
+        { noteName: 'E3', slidePosition: 7 }
+      ];
+
+      const expectedFifthPartialnotes = [
+        { noteName: 'D4', slidePosition: 1 },
+        { noteName: 'C#4', slidePosition: 2 },
+        { noteName: 'C4', slidePosition: 3 },
+        { noteName: 'B3', slidePosition: 4 },
+        { noteName: 'Bb3', slidePosition: 5 },
+        { noteName: 'A3', slidePosition: 6 },
+        { noteName: 'Ab3', slidePosition: 7 }
+      ]
+
+      expect(libBreakFinder.getAscendingLipBreaksForNote('F3'))
+        .toEqual(expect.arrayContaining([
+          {
+            slidePositionOfStartingNote: 1,
+            lipBreaks: expectedFourthPartialNotes
+          },
+          {
+            slidePositionOfStartingNote: 6,
+            lipBreaks: expectedFifthPartialnotes
+          }
+        ]));
+
+    });
+
+  })
+
 });
